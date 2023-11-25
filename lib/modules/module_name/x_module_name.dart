36d205ab6fe9d247e2bf3be1e03be7152f6e5d7e
maulahaz <maulahaz@gmail.com>
2023-11-25 11:28:33 +0400

    add CA folders

---
+++ lib/modules/module_name/x_module_name.dart
@@ -0,0 +1,13 @@
+//--Export All Module_Name files:
+
+//--CONTROLLERS:
+// export 'file_path';
+
+//--DATA AND MODELS:
+// export 'file_path';
+ 
+//--SERVICES:
+// export 'file_path';
+
+//--VIEWS AND PARTS:
+// export 'file_path';
